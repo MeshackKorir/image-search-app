f1259c9a50746bddad8b2ae02779d28cec9fb31f
Meshack Korir <meshackkorir100@gmail.com>
2023-11-06 16:49:06 +0300

    doing it

---
+++ script.js
@@ -0,0 +1,9 @@
+const accesskey = "BiY028ACynTPocJ1vywzLWGhBF9S1VEIE8hjY14wECs"
+
+const formEl = document.querySelector("form")
+const inputEl = document.querySelector("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page = 1;
