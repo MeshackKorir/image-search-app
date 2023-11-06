fcda5ae55f8cba3e2dd31d2b2424dc08c0cf280b
Meshack Korir <meshackkorir100@gmail.com>
2023-11-06 17:13:11 +0300

    src

--- script.js
+++ script.js
@@ -22,7 +22,24 @@ async function serachImages() {
     }
 
     results.map((result) => {
-        const imageWrapper = document.createElement('div')
-        imageWrapper.classList,add("search-result")
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList,add("search-result");
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        imageWrapper.appendChild(imageWrapper);
     })
+
+    page++
+
+    if(page > 1){
+        showMore.style.display = "block"
+    }
 }
